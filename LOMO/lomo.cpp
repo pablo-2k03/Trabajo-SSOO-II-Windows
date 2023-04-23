05c91aa983aa71f2e7445c96bb78bb9207b06807
Pablo Agüera Hernández <paahhe@usal.es>
2023-04-23 11:22:49 +0200

    .

--- LOMO/lomo.cpp
+++ LOMO/lomo.cpp
@@ -3,6 +3,8 @@
 #include <Windows.h>
 #include "lomo2.h"
 #include <time.h>
+#include <thread>
+#define DEBUG
 #define MAX_NTRENES 100
 //Punteros a funciones de la biblioteca de enlazado dinamico.
 typedef int(*tipoLomo_Inicio)(int, int, char const*, char const*);
@@ -27,6 +29,8 @@ DWORD WINAPI receiveThreadMessage(LPVOID param);
     int nTrenes;
     int tamMax;
     HANDLE hTrenes[MAX_NTRENES];
+    HINSTANCE libreria;
+    DWORD threadIDPadre;
 }recursosIPCS;
 
  typedef struct {
@@ -40,15 +44,15 @@ int main(int argc, char* argv[])
         fprintf(stderr, "Other Usage: lomo retardo longMax nTrenes\n");
         return -1;
     }
-    HINSTANCE libreria = LoadLibrary(TEXT("lomo2.dll"));
-    if (libreria == NULL) {
+    recursosIPCS.libreria = LoadLibrary(TEXT("lomo2.dll"));
+    if (recursosIPCS.libreria == NULL) {
         fprintf(stderr, "La biblioteca no se ha cargado.\n");
         return -2;
     }
     else {
         if (argc == 2 && !strcmp(argv[1], "--mapa")) {
             tipoLomo_Generar_Mapa puntLomoGenerarMapa;
-            if ((puntLomoGenerarMapa = (tipoLomo_Generar_Mapa)GetProcAddress(libreria, "LOMO_generar_mapa")) == NULL) {
+            if ((puntLomoGenerarMapa = (tipoLomo_Generar_Mapa)GetProcAddress(recursosIPCS.libreria, "LOMO_generar_mapa")) == NULL) {
                 fprintf(stderr, "No se ha podido generar el mapa.\n");
                 return -3;
             }
@@ -63,6 +67,9 @@ int main(int argc, char* argv[])
             return -1; 
         }
         if (comprobarPrimerArgumento(argv[1]) != -1 && comprobarSegundoArgumento(argv[2]) != -1 && comprobarTercerArgumento(argv[3]) != -1) {
+            MSG msg;
+            PeekMessage(&msg, NULL, WM_USER, WM_USER, PM_NOREMOVE); // cola de mensajes del padre..
+            recursosIPCS.threadIDPadre = GetCurrentThreadId();
             recursosIPCS.nTrenes = atoi(argv[3]);
             recursosIPCS.tamMax = atoi(argv[2]);
             if (!SetConsoleCtrlHandler((PHANDLER_ROUTINE)manejadora, TRUE)) {
@@ -81,7 +88,7 @@ int main(int argc, char* argv[])
             }
 
             tipoLomo_Inicio puntInicioLomo;
-            if ((puntInicioLomo = (tipoLomo_Inicio)GetProcAddress(libreria, "LOMO_inicio")) == NULL) {
+            if ((puntInicioLomo = (tipoLomo_Inicio)GetProcAddress(recursosIPCS.libreria, "LOMO_inicio")) == NULL) {
                 printf("Error obteniendo el puntero de LOMO_inicio.\n");
                 return -3; 
             }
@@ -89,74 +96,31 @@ int main(int argc, char* argv[])
                 fprintf(stderr, "Error en lomo inicio.\n");
                 return -4;
             }
-            int x,i;
+            int x=1,i;
             tipoMensaje tipoMensajes;
-            DWORD l;
             //Mandar mensajes de tipo x a todas las casillas.
             for (x = 1; x <= 75 * 17; x++) {
-                tipoMensajes.tipo = x;
-                l = x;
-                PostThreadMessage(l, WM_USER, WPARAM(tipoMensajes.tipo), 0);
+                       
             }
-            
+            LPDWORD l;
             for (i = 0; i < recursosIPCS.nTrenes; i++) {
-                recursosIPCS.hTrenes[i] = CreateThread(NULL, 0, receiveThreadMessage, NULL, 0, (LPDWORD)&l);
+                recursosIPCS.hTrenes[i] = CreateThread(NULL, 0, receiveThreadMessage, (LPVOID)i, 0, (LPDWORD)&l);
+                tipoMensajes.tipo = x;
                 if (recursosIPCS.hTrenes[i] == NULL) {
-                    fprintf(stderr, "Error en la creación de los hilos.\n");
+                    fprintf(stderr, "Error al obtener el HANDLE de los hilos.\n");
                     return -5;
                 }
-                tipoLomo_TrenNuevo punteroTrenNuevo;
-                punteroTrenNuevo = (tipoLomo_TrenNuevo)GetProcAddress(libreria, "LOMO_trenNuevo");
-                if (punteroTrenNuevo == NULL) {
-                    fprintf(stderr, "Error en la obtención del puntero al tren nuevo.\n");
-                    return -6;
+                PostThreadMessage(recursosIPCS.threadIDPadre, WM_USER, WPARAM(tipoMensajes.tipo), 0);
+            }
+            if (x < 75 * 17) {
+                while (x < 75 * 17) {
+                    PostThreadMessage(recursosIPCS.threadIDPadre, WM_USER, WPARAM(tipoMensajes.tipo), 0);
+                    x++;
                 }
-                int id;
-                if ((id = punteroTrenNuevo()) == -1) {
-                    fprintf(stderr, "Error en la asignación del nuevo tren.\n");
-                }
-                else {
-                    printf("El nuevo tren se ha asignado con id %d\n", id);
-                }
-                tipoLomo_PeticionAvance punteroPeticionAvance;
-                tipoLomo_Avance punteroAvance;
-                tipoLomo_LomoEspera punteroEspera;
-                int xCab, yCab;
-                int xCola, yCola;
-                int posAnterior;
-                while (1) {
-                    //LOMO_PETICIONAVANCE
-                    punteroPeticionAvance = (tipoLomo_PeticionAvance)GetProcAddress(libreria, "LOMO_peticiOnAvance");
-                    if (punteroPeticionAvance == NULL) {
-                        fprintf(stderr, "Error en la obtencion del puntero de avance del tren.\n");
-                        return -1;
-                    }
-                    if (punteroPeticionAvance(id, &xCab, &yCab) == -1) {
-                        fprintf(stderr, "No se ha podido solicitar la peticion de avance.\n");
-                    }
-                    printf("Cabina: %d %d\n", xCab, yCab);
-                    //LOMO_AVANCE
-                    punteroAvance = (tipoLomo_Avance)GetProcAddress(libreria, "LOMO_avance");
-                    if (punteroAvance == NULL) {
-                        fprintf(stderr, "Error en la obtencion del puntero de avance del tren.\n");
-                        return -1;
-                    }
-                    if (punteroAvance(id, &xCola, &yCola) == -1) {
-                        fprintf(stderr, "No se ha podido solicitar la peticion de avance.\n");
-                    }
-                    printf("\nCola: %d %d", xCola, yCola);
-
-                    posAnterior = yCola;
-                    //LOMO_ESPERA
-                    punteroEspera = (tipoLomo_LomoEspera)GetProcAddress(libreria, "LOMO_espera");
-                    if (punteroEspera == NULL) {
-                        fprintf(stderr, "Error en la obtencion del puntero de avance del tren.\n");
-                        return -1;
-                    }
-                    punteroEspera(posAnterior, yCab);
-                }
-            }  
+            }
+            WaitForMultipleObjects(recursosIPCS.nTrenes, recursosIPCS.hTrenes, true, INFINITE);
         }
+        
         else {
             fprintf(stderr, "Introduzca unos argumentos validos.\n");
             return -1;
@@ -206,9 +170,68 @@ BOOL WINAPI manejadora(DWORD param) {
 
 }
 DWORD WINAPI receiveThreadMessage(LPVOID param) {
-    MSG msg;
-    PeekMessage(&msg, NULL, WM_USER, WM_USER, PM_NOREMOVE); // cola de mensajes.
-    GetMessage(&msg, NULL, 0, 0); // leer mensaje.
-    printf("%d\n", msg.wParam);
+    
+    MSG msg;    
+    tipoMensaje tipo;
+    DWORD l=1;
+    PeekMessage(&msg, NULL, WM_USER, WM_USER, PM_NOREMOVE); // cola de mensajes del hilo.
+    tipoLomo_TrenNuevo punteroTrenNuevo;
+    punteroTrenNuevo = (tipoLomo_TrenNuevo)GetProcAddress(recursosIPCS.libreria, "LOMO_trenNuevo");
+    if (punteroTrenNuevo == NULL) {
+        fprintf(stderr, "Error en la obtención del puntero al tren nuevo.\n");
+        return -6;
+    }
+    int id;
+    if ((id = punteroTrenNuevo()) == -1) {
+        fprintf(stderr, "Error en la asignación del nuevo tren.\n");
+    }
+    else {
+        printf("El nuevo tren se ha asignado con id %d\n", id);
+    }
+    tipoLomo_PeticionAvance punteroPeticionAvance;
+    tipoLomo_Avance punteroAvance;
+    tipoLomo_LomoEspera punteroEspera;
+    int xCab, yCab;
+    int xCola, yCola;
+    int posAnterior;
+    while (1) {
+        //LOMO_PETICIONAVANCE
+        punteroPeticionAvance = (tipoLomo_PeticionAvance)GetProcAddress(recursosIPCS.libreria, "LOMO_peticiOnAvance");
+        if (punteroPeticionAvance == NULL) {
+            fprintf(stderr, "\nError en la obtencion del puntero de avance del tren.");
+            return -7;
+        }
+        if (punteroPeticionAvance(id, &xCab, &yCab) == -1) {
+            fprintf(stderr, "\nNo se ha podido solicitar la peticion de avance.");
+        }
+        printf("\nCabina: %d %d\n", xCab, yCab);
+        //LOMO_AVANCE
+        punteroAvance = (tipoLomo_Avance)GetProcAddress(recursosIPCS.libreria, "LOMO_avance");
+        if (punteroAvance == NULL) {
+            fprintf(stderr, "\nError en la obtencion del puntero de avance del tren.");
+            return -8;
+        }
+        if (punteroAvance(id, &xCola, &yCola) == -1) {
+            fprintf(stderr, "\nNo se ha podido solicitar la peticion de avance.");
+        }
+        printf("\nCola: %d %d", xCola, yCola);
+        posAnterior = yCola;
+        //LOMO_ESPERA
+        punteroEspera = (tipoLomo_LomoEspera)GetProcAddress(recursosIPCS.libreria, "LOMO_espera");
+        if (punteroEspera == NULL) {
+            fprintf(stderr, "\nError en la obtencion del puntero de avance del tren.");
+            return -9;
+        }
+        punteroEspera(posAnterior, yCab);
+        if (GetMessage(&msg, NULL, 0, 0) == -1) { // leer mensaje.
+            fprintf(stderr, "\nError en la lectura del mensaje.");
+            return -10;
+        } 
+        printf("%d", msg.wParam);
+        if (msg.wParam >= 0) {
+            tipo.tipo = yCab * 75 + xCola + 1;
+            PostThreadMessage(recursosIPCS.threadIDPadre, WM_USER, WPARAM(tipo.tipo), 0);
+        }
+    }
     return 1;
 }
